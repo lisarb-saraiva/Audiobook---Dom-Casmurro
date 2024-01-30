4e1221cd5890429274ba12fa90e5f9ff07dbead1
Antônio Lisarb Cordeiro Saraiva <155826745+lisarb-saraiva@users.noreply.github.com>
2024-01-30 09:07:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ const audio = document.getElementById("audio-capitulo");
 const botaoPlayPause = document.getElementById("play-pause");
 const botaoProximoCapitulo = document.getElementById("proximo");
 const botaoCapituloAnterior = document.getElementById("anterior");
+const tempoAtual = document.getElementById("tempo-atual");
+const barraProgresso = document.getElementById("barra-progresso");
 
 const quantidadeCapitulos = 10;
 let taTocando = false;
 let capitulo = 1;
+
+function atualizarTempoAtual() {
+  const horas = Math.floor(audio.currentTime / 3600);
+  const minutos = Math.floor((audio.currentTime % 3600) / 60);
+  const segundos = Math.floor(audio.currentTime % 60);
+  tempoAtual.innerText = horas.toString().padStart(2, "0") + ":" + minutos.toString().padStart(2, "0") + ":" + segundos.toString().padStart(2, "0");
+  
+  const porcentagemProgresso = (audio.currentTime / audio.duration) * 100;
+  barraProgresso.value = porcentagemProgresso;
+}
+
+function selecionarMinuto(event) {
+  const larguraBarra = this.offsetWidth;
+  const clickX = event.offsetX;
+  const porcentagemSelecionada = (clickX / larguraBarra) * 100;
+  const tempoTotal = audio.duration;
+  const tempoSelecionado = (porcentagemSelecionada / 100) * tempoTotal;
+  audio.currentTime = tempoSelecionado;
+}
 
 function tocarFaixa() {
   botaoPlayPause.classList.remove("bi-play-circle-fill");
@@ -56,4 +77,6 @@ botaoPlayPause.addEventListener("click", tocarOuPausarFaixa);
 botaoCapituloAnterior.addEventListener("click", capituloAnterior);
 botaoProximoCapitulo.addEventListener("click", proximoCapitulo);
 audio.addEventListener("ended", proximoCapitulo);
+barraProgresso.addEventListener("input", selecionarMinuto);
+audio.addEventListener("timeupdate", atualizarTempoAtual);
 
