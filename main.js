1981a143b885812d22469099afac1bc006c2ef77
Antônio Lisarb Cordeiro Saraiva <155826745+lisarb-saraiva@users.noreply.github.com>
2024-01-30 10:55:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,25 @@ botaoPlayPause.classList.remove("bi-play-circle-fill");
 botaoPlayPause.classList.add("bi-pause-circle-fill");
 audio.play();
 taTocando = true;
+audio.addEventListener("timeupdate", function() {
+    const tempoAtual = audio.currentTime;
+    const horas = Math.floor(tempoAtual / 3600);
+    const minutos = Math.floor((tempoAtual % 3600) / 60);
+    const segundos = Math.floor(tempoAtual % 60);
+    const tempoFormatado = horas.toString().padStart(2, '0') + ':' + minutos.toString().padStart(2, '0') + ':' + segundos.toString().padStart(2, '0');
+    const duracaoTotal = audio.duration;
+    const porcentagem = (tempoAtual / duracaoTotal) * 100;
+    document.getElementById("barra-tempo").value = porcentagem;
+    document.getElementById("tempo-atual").innerText = tempoFormatado;
+  });
 }
+
+document.getElementById("barra-tempo").addEventListener("input", function() {
+    const porcentagem = this.value;
+    const duracaoTotal = audio.duration;
+    const novoTempo = (porcentagem / 100) * duracaoTotal;
+    audio.currentTime = novoTempo;
+  });
 
 function pausarFaixa() {
 botaoPlayPause.classList.add("bi-play-circle-fill");
@@ -39,6 +57,7 @@ capitulo -= 1;
 audio.src = "books/dom-casmurro/" + capitulo + ".mp3";
 nomeCapitulo.innerText = "Capítulo " + capitulo;
 tocarFaixa();
+document.getElementById("barra-tempo").value = 0;
 }
 
 function proximoCapitulo() {
@@ -50,9 +69,11 @@ capitulo = 1;
 audio.src = "books/dom-casmurro/" + capitulo + ".mp3";
 nomeCapitulo.innerText = "Capítulo " + capitulo;
 tocarFaixa();
+document.getElementById("barra-tempo").value = 0;
 }
 
 botaoPlayPause.addEventListener("click", tocarOuPausarFaixa);
 botaoCapituloAnterior.addEventListener("click", capituloAnterior);
 botaoProximoCapitulo.addEventListener("click", proximoCapitulo);
 audio.addEventListener("ended", proximoCapitulo);
+
